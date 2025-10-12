9efdb09f60aa12fd8b54127d55a1b992f1bced3c
talha@6sensehq <talha@6sensehq.com>
2025-10-12 21:54:27 +0600

    theme fixed

--- components/ShopByTheme.vue
+++ components/ShopByTheme.vue
@@ -7,10 +7,23 @@
         </section>
         <section>
             <div class="theme-grid">
-                <img src="/theme-1.png" alt="Theme Rang" class="theme-image"></img>
-                <img src="/theme-2.png" alt="Theme Rang" class="theme-image"></img>
-                <img src="/theme-3.png" alt="Theme Rang" class="theme-image"></img>
-                <img src="/theme-4.png" alt="Theme Rang" class="theme-image"></img>
+                <div 
+                    v-for="(theme, index) in themeImages" 
+                    :key="index"
+                    class="theme-image-container"
+                >
+                    <img 
+                        :src="theme.src" 
+                        :alt="theme.alt" 
+                        class="theme-image"
+                    />
+                    <div class="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/70 to-transparent p-4 rounded-b-lg">
+                        <h3 class="theme-name-light">{{ theme.title }}</h3>
+                        <p class="shop-now-blue-button-div">
+                            <ShopNowBlue />
+                        </p>
+                    </div>
+                </div>
             </div>
             
         </section>
@@ -18,7 +31,30 @@
 </template>
 
 <script setup lang="ts">
+import ShopNowBlue from './ShopNowBlue.vue'
 
+const themeImages = [
+    {
+        src: '/theme/theme-1.png',
+        alt: 'Theme Rang',
+        title: 'Wedding'
+    },
+    {
+        src: '/theme/theme-2.png',
+        alt: 'Theme Rang',
+        title: 'EID'
+    },
+    {
+        src: '/theme/theme-1.png',
+        alt: 'Theme Rang',
+        title: 'Gaye Holud'
+    },
+    {
+        src: '/theme/theme-2.png',
+        alt: 'Theme Rang',
+        title: 'Durga Puja'
+    }
+]
 </script>
 
 <style scoped>
