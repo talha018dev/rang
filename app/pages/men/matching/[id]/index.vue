ea831732f5023a19953aee0e2c2b0cd2c4deecec
talha@6sensehq <talha@6sensehq.com>
2025-10-12 22:02:54 +0600

    radio button added

--- app/pages/men/matching/[id]/index.vue
+++ app/pages/men/matching/[id]/index.vue
@@ -98,14 +98,21 @@
         <!-- Size Selection -->
         <div class="size-selection">
           <label class="selection-label">Size</label>
-          <div class="size-dropdown">
-            <select v-model="selectedSize" class="size-select">
-              <option value="">Select</option>
-              <option v-for="size in product.sizes" :key="size" :value="size">{{ size }}</option>
-            </select>
-            <svg class="dropdown-icon" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-            </svg>
+          <div class="size-radio-group">
+            <label 
+              v-for="size in product.sizes" 
+              :key="size" 
+              class="size-radio-option"
+              :class="{ selected: selectedSize === size }"
+            >
+              <input 
+                type="radio" 
+                :value="size" 
+                v-model="selectedSize" 
+                class="size-radio-input"
+              />
+              <span class="size-radio-label">{{ size }}</span>
+            </label>
           </div>
           <NuxtLink to="#" class="size-guide-link">See Sizes & Fit Details</NuxtLink>
         </div>
