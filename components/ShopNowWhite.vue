66370185b8b64a70517d787fe2c79d8daf2a87eb
talha@6sensehq <talha@6sensehq.com>
2025-10-12 21:31:36 +0600

    white button added

---
+++ components/ShopNowWhite.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="shop-now-white-button">
+    <span class="button-text">Shop Now</span>
+    <Icon name="heroicons:shopping-bag" class="shopping-bag-icon" />
+  </button>
+</template>
+
+<script setup lang="ts">
+// Component logic can be added here if needed
+import './ShopNowWhite.css';
+</script>
