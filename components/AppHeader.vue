c2ab3449a3cfb586739a0fd8f8abc081d4cf8031
talha@6sensehq <talha@6sensehq.com>
2025-11-18 23:35:48 +0600

    Add utility class .py-0 for padding control and update AppHeader button class
    - Introduced a new utility class .py-0 to set top and bottom padding to 0 for improved layout flexibility.
    - Updated the AppHeader component to use the new .py-0 class for better styling consistency in the action button.

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -145,7 +145,7 @@
             </button>
           </NuxtLink> -->
           <NuxtLink to="/store-location-rang" class="action-button p-0 color-inherit store-locator-link">
-            <button class="action-button p-0 color-inherit">
+            <button class="action-button py-0 color-inherit">
               <!-- <NuxtImg src="/store-locator-2.png" alt="Store Locator" class="action-icon store-locator-icon"
                 style="filter: brightness(1) invert(0); width: 24px; height: 34px;" format="webp" quality="85"
                 loading="lazy" /> -->
