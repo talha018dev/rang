daf17fb931a68fb7871f927daffc3977b9719f56
talha@6sensehq <talha@6sensehq.com>
2025-11-19 23:50:23 +0600

    Enhance CSS styles and structure for ExploreRang component
    - Updated the layout of the ExploreRang component to improve image positioning and responsiveness.
    - Introduced new utility classes for absolute positioning of elements.
    - Adjusted media queries for better text sizing on smaller screens.
    - Added new images to the explore section for enhanced visual content.
    - Refined CSS properties for frequently bought items to ensure consistent sizing and aspect ratios.

--- components/ExploreRang.vue
+++ components/ExploreRang.vue
@@ -12,30 +12,62 @@
                     <div class="image-div explore-grid-main-image">
                         <NuxtImg src="/explore/explore-1.png" alt="Explore Rang" class="category-image-rounded"
                             style="height: 100%;" format="webp" quality="85" loading="lazy" />
-                        <div class="image-div-text bottom-left">
-                            <div class="small-image-text">See what’s new in our</div>
+                        <div class="image-div-text top-left">
                             <div>Family</div>
-                            <p>Collection</p>
                         </div>
-                        <div class="image-shop-now">Shop Now
-                            <Icon name="heroicons:chevron-right" />
+                        <div class="bottom-right absolute">
+                            <ShopNowBlue />
                         </div>
                     </div>
                 </NuxtLink>
                 <NuxtLink to="/products/jewelry">
-                    <NuxtImg src="/explore-2.png" alt="Explore Rang" class="explore-image-rounded" format="webp" quality="85" loading="lazy" />
+                    <div class="image-div">
+                        <NuxtImg src="/explore/explore-22.png" alt="Explore Rang" class="explore-image-rounded"
+                            format="webp" quality="85" loading="lazy" />
+                        <div class="image-div-text top-left">
+                            <div>Jewelry</div>
+                        </div>
+                        <div class="bottom-right absolute">
+                            <ShopNowBlue />
+                        </div>
+                    </div>
                 </NuxtLink>
             </div>
             <div class="explore-grid-secondary">
-                <NuxtLink to="/products/matching">
-                    <NuxtImg src="/explore-3.png" alt="Explore Rang" class="explore-image" format="webp" quality="85" loading="lazy" />
+                <NuxtLink to="/products/matching" style="height: 600px;">
+                    <div class="image-div">
+                        <NuxtImg src="/explore/explore-33.jpg" alt="Explore Rang" class="explore-image-rounded"
+                            format="webp" quality="85" loading="lazy" />
+                        <div class="image-div-text top-left">
+                            <div>Matching</div>
+                        </div>
+                        <div class="bottom-right absolute">
+                            <ShopNowBlue />
+                        </div>
+                    </div>
                 </NuxtLink>
-                <NuxtLink to="/products/couple">
-                    <NuxtImg src="/explore-44.png" alt="Explore Rang" class="explore-image" format="webp" quality="85" loading="lazy" />
+                <NuxtLink to="/products/couple" style="height: 600px;">
+                    <div class="image-div">
+                        <NuxtImg src="/explore/explore-44.png" alt="Explore Rang" class="explore-image-rounded"
+                            format="webp" quality="85" loading="lazy" />
+                        <div class="image-div-text top-left">
+                            <div>Couple</div>
+                        </div>
+                        <div class="bottom-right absolute">
+                            <ShopNowBlue />
+                        </div>
+                    </div>
                 </NuxtLink>
-                <NuxtLink to="/products/gifts-crafts">
-                    <div class="explore-image-container">
-                        <NuxtImg src="/explore-55.png" alt="Explore Rang" class="explore-image-rounded" format="webp" quality="85" loading="lazy" />
+                <NuxtLink to="/products/gifts-crafts" style="height: 600px;">
+                    <div class="image-div">
+                        <NuxtImg src="/explore/explore-55.png" alt="Explore Rang" class="explore-image-rounded"
+                            format="webp" quality="85" loading="lazy" />
+                        <div class="image-div-text top-left">
+                            <div>Gifts & Crafts</div>
+                        </div>
+                        <div class="bottom-right absolute">
+                            <ShopNowBlue />
+                        </div>
                     </div>
                 </NuxtLink>
             </div>
@@ -44,7 +76,7 @@
 </template>
 
 <script setup lang="ts">
-
+import ShopNowBlue from './ShopNowBlue.vue';
 </script>
 
 <style scoped>
