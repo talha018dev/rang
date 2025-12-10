82ff4dda089d1719abb11bf683c66a9156fa6807
talha@6sensehq <talha@6sensehq.com>
2025-12-11 00:05:56 +0600

    Update ShopByCategory component for improved text positioning and styling
    - Adjusted the positioning of category text overlays for better alignment and visibility.
    - Changed font sizes for the "Shop Now" text to enhance readability across different screen sizes.

--- components/ShopByCategory.vue
+++ components/ShopByCategory.vue
@@ -17,8 +17,8 @@
                         quality="85" 
                         loading="lazy" 
                     />
-                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-10 left-10 z-20 explore-rang-top-text">Men</div>
-                    <div class="explore-rang-bottom-text text-lg! md:text-xl! lg:text-2xl! absolute bottom-10 right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
+                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-2 left-2 md:top-10 md:left-10 z-20 explore-rang-top-text">Men</div>
+                    <div class="explore-rang-bottom-text text-sm! md:text-xl! lg:text-2xl! absolute bottom-2 right-2 md:bottom-10 md:right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
                 </NuxtLink>
                 <NuxtLink to="/products/women" class="relative flex-[1.5] aspect-[1.73]">
                     <NuxtImg 
@@ -29,8 +29,8 @@
                         quality="85" 
                         loading="lazy" 
                     />
-                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl!  top-10 left-10 z-20 explore-rang-top-text">Women</div>
-                    <div class="explore-rang-bottom-text text-lg! md:text-xl! lg:text-2xl! absolute bottom-10 right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
+                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl!  top-2 left-2 md:top-10 md:left-10 z-20 explore-rang-top-text">Women</div>
+                    <div class="explore-rang-bottom-text text-sm! md:text-xl! lg:text-2xl! absolute bottom-2 right-2 md:bottom-10 md:right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
                 </NuxtLink>
             </div>
             <div class="flex gap-2">
@@ -43,8 +43,8 @@
                         quality="85" 
                         loading="lazy" 
                     />
-                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-10 left-10 z-20 explore-rang-top-text">Kids</div>
-                    <div class="explore-rang-bottom-text text-lg! md:text-xl! lg:text-2xl! absolute bottom-10 right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
+                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-2 left-2 md:top-10 md:left-10 z-20 explore-rang-top-text">Kids</div>
+                    <div class="explore-rang-bottom-text text-sm! md:text-xl! lg:text-2xl! absolute bottom-2 right-2 md:bottom-10 md:right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
                 </NuxtLink>
                 <NuxtLink to="/products/home-decor" class="relative flex-[1] aspect-[0.82]">
                     <NuxtImg 
@@ -56,8 +56,8 @@
                         quality="85" 
                         loading="lazy" 
                     />
-                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-10 left-10 z-20 explore-rang-top-text">Home Decor</div>
-                    <div class="explore-rang-bottom-text text-lg! md:text-xl! lg:text-2xl! absolute bottom-10 right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
+                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-2 left-2 md:top-10 md:left-10 z-20 explore-rang-top-text">Home Decor</div>
+                    <div class="explore-rang-bottom-text text-sm! md:text-xl! lg:text-2xl! absolute bottom-2 right-2 md:bottom-10 md:right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
                 </NuxtLink>
                 <NuxtLink to="/products/accessories" class="relative flex-[1] aspect-[0.82]">
                     <NuxtImg 
@@ -69,8 +69,8 @@
                         quality="85" 
                         loading="lazy" 
                     />
-                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-10 left-10 z-20 explore-rang-top-text">Accessories</div>
-                    <div class="explore-rang-bottom-text text-lg! md:text-xl! lg:text-2xl! absolute bottom-10 right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
+                    <div class="absolute text-2xl! md:text-3xl! lg:text-6xl! top-2 left-2 md:top-10 md:left-10 z-20 explore-rang-top-text">Accessories</div>
+                    <div class="explore-rang-bottom-text text-sm! md:text-xl! lg:text-2xl! absolute bottom-2 right-2 md:bottom-10 md:right-10 z-20 cursor-pointer text-white hover:text-[#ea580c]">Shop Now</div>
                 </NuxtLink>
             </div>
         </section>
